c1667112839b3779e1f81d546f92f092e1423522
michelle5434123 <151757868+michelle5434123@users.noreply.github.com>
2025-08-04 23:35:29 +0200

    Update Footer, App(updated operator), and styling

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+export default function App() {
+    const [count, setCount] = useState(0)
+
+    function add(){
+        setCount(function(){
+            return count+1;
+        })
+    }
+
+    function subtract(){
+        setCount(prevCount => prevCount-1)}
+
+    return (
+        <main className="container">
+            <h2>People entered:</h2>
+            <div className="counter">
+                <button className="Decrease-btn" onClick={subtract} aria-label="Decrease count">-</button>
+                <h3 className="count">{count}</h3>
+                <button className="increase-btn" onClick={add} aria-label="Increase count">+</button>
+            </div>
+        </main>
+  )
+}
---
+++ Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <small>© 2025 YuHua Huang React-based development. </small>
+        </footer>
+    )
+}
